e504086b06b3333def5881bfc62abd07bb97196a
Kaushil <backs.kaushil@gmail.com>
2022-01-17 17:49:19 -0500

    Initial Commit

---
+++ Lab1_KaushilPrajapati_c0826985_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab1_KaushilPrajapati_c0826985_iOS
+//
+//  Created by Kaushil Prajapati on 17/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
